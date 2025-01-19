59e5cdca7e5c06c6cf21657a67f6b6eccd102354
Серік Дінмұхамед Болатұлы <dinmukhamed.serik@bcc.kz>
2025-01-19 12:41:35 +0500

    fix:reset-migragtion

--- prisma/migrations/20250118205639_d/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `phoneNumber` to the `Account` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Account" ADD COLUMN     "level" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "phoneNumber" TEXT NOT NULL;
